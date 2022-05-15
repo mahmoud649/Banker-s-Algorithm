fb6cc2232e67b514ee18dfde0b960d5978ac9fff
AdelHashem <96683697+AdelHashem@users.noreply.github.com>
2022-05-15 17:40:09 +0200

    Add subtract_matrices Fun
    Add subtract_matrices Fun and Get the Need Recourses matrix

--- banker.c
+++ banker.c
@@ -5,6 +5,7 @@
 
 void LoadFile(char *); //Load File In Path Variable
 void GetData(int n,int c[][3]); //Get Data From File   n:number of rows to get
+void subtract_matrices(int rows,int col,int out[][col],int mat1[][col],int mat2[][col]);
 FILE *f; //carry the file pointer
  
 
@@ -13,10 +14,12 @@ int main(){
 char path[256];
 LoadFile(path);
 f = fopen(path,"r");
-int c[5][3],M[5][3],A[1][3];
-GetData(5,c);
+int C[5][3],M[5][3],A[1][3];
+GetData(5,C);
 GetData(5,M);
 GetData(1,A);
+int need[5][3];
+subtract_matrices(5,3,need,M,C);
 
 printf("\n\n %d %d %d",A[0][0],A[0][1],A[0][2]);
 
@@ -44,4 +47,13 @@ void GetData(int n,int c[][3])
         i++;
     }
 
+}
+
+void subtract_matrices(int rows,int col,int out[][col],int mat1[][col],int mat2[][col])
+{
+    // subtract two matrices
+  for (int i = 0; i < rows; ++i)
+    for (int j = 0; j < col; ++j) {
+      out[i][j] = mat1[i][j] - mat2[i][j];
+    }
 }
